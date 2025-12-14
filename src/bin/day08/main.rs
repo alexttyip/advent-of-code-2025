98e4c3394478ba506947e42bf8fdfe9a3089835b
Alex Yip <tsztoyip@gmail.com>
2025-12-14 21:51:38 +0000

    Day 8 wip

--- src/bin/day08/main.rs
+++ src/bin/day08/main.rs
@@ -1,97 +1,67 @@
 #![feature(test)]
 
 use itertools::Itertools;
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
-use std::mem::swap;
+use std::array::from_fn;
+use std::iter::FlatMap;
 use std::time::Instant;
 
-fn get_distances() -> BinaryHeap<(Reverse<u64>, ((u64, u64, u64), (u64, u64, u64)))> {
-    include_str!("input.txt")
-        .lines()
-        .map(|line| -> (_, _, _) {
-            line.split(',')
-                .map(|n| n.parse::<u64>().unwrap())
-                .collect_tuple()
-                .unwrap()
-        })
-        .combinations(2)
-        .map(|comb| {
-            let point1 = comb[0];
-            let point2 = comb[1];
+const N: usize = 1000;
 
-            let dist = point1.0.abs_diff(point2.0).pow(2)
-                + point1.1.abs_diff(point2.1).pow(2)
-                + point1.2.abs_diff(point2.2).pow(2);
+fn find(roots: &mut [usize], x: usize) -> usize {
+    if roots[x] == x {
+        return x;
+    }
 
-            (Reverse(dist), (point1, point2))
-        })
-        .collect()
+    roots[x] = find(roots, roots[x]);
+    roots[x]
 }
 
-fn make_connection(
-    connections: &mut Vec<Vec<(u64, u64, u64)>>,
-    point1: (u64, u64, u64),
-    point2: (u64, u64, u64),
-) {
-    let p1_cs = connections.iter().position(|cs| cs.contains(&point1));
-    let p2_cs = connections.iter().position(|cs| cs.contains(&point2));
-
-    match (p1_cs, p2_cs) {
-        (Some(mut p1_cs), Some(mut p2_cs)) => {
-            if p1_cs != p2_cs {
-                if p1_cs > p2_cs {
-                    swap(&mut p1_cs, &mut p2_cs);
-                }
-
-                let p2 = connections.remove(p2_cs);
-
-                connections.get_mut(p1_cs).unwrap().extend(p2);
-            }
-        }
-        (Some(p1_cs), None) => {
-            connections.get_mut(p1_cs).unwrap().push(point2);
-        }
-        (None, Some(p2_cs)) => {
-            connections.get_mut(p2_cs).unwrap().push(point1);
-        }
-        _ => {
-            connections.push(vec![point1, point2]);
-        }
-    }
+fn union(roots: &mut [usize], x: usize, y: usize) {
+    let x_root = find(roots, x);
+    let y_root = find(roots, y);
+    roots[x_root] = y_root;
 }
 
 fn part1() -> u64 {
-    let mut distances = get_distances();
-    let mut connections: Vec<Vec<(_, _, _)>> = vec![];
+    let mut tuples = include_str!("input.txt").lines().flat_map(|line| {
+        line.split(',')
+            .flat_map(|s| s.parse::<u64>())
+            .collect_tuple::<(_, _, _)>()
+    });
 
-    for _ in 0..1000 {
-        let (_, (point1, point2)) = distances.pop().unwrap();
-        make_connection(&mut connections, point1, point2);
+    let points: [(_, _, _); N] = from_fn(|_| tuples.next().unwrap());
+
+    let mut roots: [usize; N] = from_fn(|i| i);
+
+    points
+        .iter()
+        .enumerate()
+        .combinations(2)
+        .map(|points| {
+            let (i1, (x1, y1, z1)) = points[0];
+            let (i2, (x2, y2, z2)) = points[1];
+
+            let d = x1.abs_diff(*x2).pow(2) + y1.abs_diff(*y2).pow(2) + z1.abs_diff(*z2).pow(2);
+
+            (d, i1, i2)
+        })
+        .sorted_unstable()
+        .take(1000)
+        .for_each(|(_, x, y)| union(&mut roots, x, y));
+
+    let mut sizes = [0; N];
+
+    for x in 0..N {
+        sizes[find(&mut roots, x)] += 1;
     }
 
-    connections
-        .iter()
-        .map(|v| v.len())
-        .sorted_unstable()
-        .rev()
-        .take(3)
-        .product::<usize>() as u64
+    sizes.iter().sorted_unstable().rev().take(3).product()
 }
 
 fn part2() -> u64 {
-    let mut distances = get_distances();
-    let mut connections: Vec<Vec<(_, _, _)>> = vec![];
+    let lines = include_str!("input.txt").lines();
 
-    while let Some((_, (point1, point2))) = distances.pop() {
-        make_connection(&mut connections, point1, point2);
-
-        if connections.iter().map(|v| v.len()).any(|len| len == 1000) {
-            return point1.0 * point2.0;
-        }
-    }
-
-    panic!();
+    0
 }
 
 pub fn main() {
@@ -109,8 +79,8 @@ pub fn main() {
     println!("Part 1 took: {:.2?}", part1_elapsed);
     println!("Part 2 took: {:.2?}", part2_elapsed);
 
-    assert_eq!(part1, 32103);
-    assert_eq!(part2, 8133642976);
+    // assert_eq!(part1, 0);
+    // assert_eq!(part2, 0);
 }
 
 #[cfg(test)]
